298352fa483013726a46f35072cc670d93ea59ca
zhaojuntong <zhaojt@77ircloud.com>
2019-12-17 23:28:39 +0800

    feat: compile info

--- scripts/build.js
+++ scripts/build.js
@@ -2,6 +2,8 @@ process.env.NODE_ENV = 'production';
 
 const webpack = require('webpack');
 const fs = require('fs-extra');
+const chalk = require('chalk');
+const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
 const { appBuild, public, templateHtml } = require('../webpack/paths');
 const webpackConfig = require('../webpack/webpack.config');
 
@@ -19,7 +21,41 @@ function build() {
   copyPublicFolder();
   const complier = webpack(webpackConfig);
   complier.run((err, stats) => {
-    console.log(err, stats);
+    let messages;
+    if (err) {
+      if (!err.message) {
+        console.log(chalk.red())
+        return new Error(err);
+      }
+      messages = formatWebpackMessages({
+        errors: [err.message],
+        warnings: []
+      });
+    } else {
+      const statsJson = stats.toJson({
+        all: false,
+        warnings: true,
+        errors: true,
+        timings: true
+      });
+      messages = formatWebpackMessages(statsJson);
+      const isComplieSuccessfully = !messages.warnings.length && !messages.errors.length;
+      if (isComplieSuccessfully) {
+        console.log(chalk.green(`打包成功！compile time: ${(statsJson.time/1000).toFixed(2)}s`))
+      }
+    }
+    if (messages.errors.length) {
+      messages.errors.forEach(message => {
+        console.log(chalk.red(message));
+        console.log();
+      });
+    }
+    if (messages.warnings.length) {
+      messages.warnings.forEach(message => {
+        console.log(chalk.yellow(message));
+        console.log();
+      });
+    }
   });
 };
 
